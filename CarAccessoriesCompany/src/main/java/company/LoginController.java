1e7a1e9ab47cbc30b1f2408e41367f8649c80879
RihamKatout <54861182+RihamKatout@users.noreply.github.com>
2023-11-02 14:33:09 +0200

    add login controller

---
+++ CarAccessoriesCompany/src/main/java/company/LoginController.java
@@ -0,0 +1,31 @@
+package company;
+
+import javafx.fxml.FXML;
+import javax.swing.*;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+
+    @FXML
+    private PasswordField passwordTextField;
+
+
+    @FXML
+    private TextField usernameTextField;
+
+    @FXML
+    protected void onLoginClick() {
+        //TODO
+        JOptionPane.showMessageDialog(null, "Hello!");
+    }
+
+    @FXML
+    protected void onSignupClick(){
+        //TODO
+    }
+    @FXML
+    protected void onForgetPasswordClick(){
+        //TODO
+    }
+}
