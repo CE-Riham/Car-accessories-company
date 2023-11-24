9ece259c3d66fc373a6d3b89a8412adc71eb3ba5
RihamKatout <54861182+RihamKatout@users.noreply.github.com>
2023-11-24 20:53:00 +0200

    add login implementation

---
+++ CarAccessoriesCompany/src/main/java/Classes/UserSession.java
@@ -0,0 +1,34 @@
+package Classes;
+
+import javafx.scene.Parent;
+import model.User;
+
+public class UserSession extends User {
+    private String sessionId;
+
+    public UserSession(User user){
+        setUsername(user.getUsername());
+        setFirstName(user.getFirstName());
+        setLastName(user.getLastName());
+        setEmail(user.getEmail());
+        setPassword(user.getPassword());
+        setPhoneNumber(user.getPhoneNumber());
+        setAddress(user.getAddress());
+        setImagePath(user.getImagePath());
+    }
+
+    public String getSessionId() {
+        return sessionId;
+    }
+
+    public void setSessionId(String sessionId) {
+        this.sessionId = sessionId;
+    }
+
+    @Override
+    public String toString() {
+        return "UserSession{" + getUsername() + ": " +
+                "sessionId='" + sessionId + '\'' +
+                '}';
+    }
+}
