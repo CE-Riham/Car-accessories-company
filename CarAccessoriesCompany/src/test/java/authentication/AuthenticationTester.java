72ea25bc5211191583688bd17cd873eb31e9aa84
RihamKatout <54861182+RihamKatout@users.noreply.github.com>
2023-12-23 00:18:56 +0200

    edit tester name

--- CarAccessoriesCompany/src/test/java/authentication/AuthenticationTester.java
+++ CarAccessoriesCompany/src/test/java/authentication/AuthenticationTester.java
@@ -7,5 +7,5 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/resources/authenticationFeatures",
         monochrome = true,snippets = CucumberOptions.SnippetType.CAMELCASE, glue = {"authentication"})
-public class AuthenticationTest {
+public class AuthenticationTester {
 }
