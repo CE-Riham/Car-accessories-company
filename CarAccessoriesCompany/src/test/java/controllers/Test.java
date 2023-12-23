09ab122468f1dc32f7c6a2f9c172b53af8b025a6
ShahdNoman <143310993+ShahdNoman@users.noreply.github.com>
2023-12-23 18:35:29 +0200

    Update Test.java

--- CarAccessoriesCompany/src/test/java/controllers/Test.java
+++ CarAccessoriesCompany/src/test/java/controllers/Test.java
@@ -1,11 +1,11 @@
-//package controllers;
-//
-//import io.cucumber.junit.Cucumber;
-//import io.cucumber.junit.CucumberOptions;
-//import io.cucumber.junit.CucumberOptions.SnippetType;
-//import org.junit.runner.RunWith;
-//
-//@RunWith(Cucumber.class)
-//@CucumberOptions(features = "src/test/resources/cases", monochrome = true,snippets = SnippetType.CAMELCASE, glue = {"controllers"})
-//public class Test {
-//}
+package controllers;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import io.cucumber.junit.CucumberOptions.SnippetType;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/resources/cases", monochrome = true,snippets = SnippetType.CAMELCASE, glue = {"controllers"})
+public class Test {
+}
