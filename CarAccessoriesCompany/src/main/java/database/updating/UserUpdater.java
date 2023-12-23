34c3b3b5291e7326678eecc6a96be38e3befdbb9
ShahdNoman <143310993+ShahdNoman@users.noreply.github.com>
2023-12-23 20:05:12 +0200

    Update UserUpdater.java

--- CarAccessoriesCompany/src/main/java/database/updating/UserUpdater.java
+++ CarAccessoriesCompany/src/main/java/database/updating/UserUpdater.java
@@ -35,51 +35,56 @@ public class UserUpdater {
         return false;
     }
 
-    public boolean updateUsersAllFields(User user, String condition) {
-      String st = DataValidation.userValidationTest(user);
-        if (st.equals("Valid")) {
-            String query = "UPDATE users SET firstName = ?, lastName = ?, username = ?, " +
-                    "phone = ?, email = ?, userPassword = ?, image = ?, userType = ? " + condition;
-            try (PreparedStatement preparedStmt = connection.prepareStatement(query)) {
-                preparedStmt.setString(1, user.getFirstName());
-                preparedStmt.setString(2, user.getLastName());
-                preparedStmt.setString(3, user.getUsername());
-                preparedStmt.setString(4, user.getPhoneNumber());
-                preparedStmt.setString(5, user.getEmail());
-                preparedStmt.setString(6, user.getPassword());
-                preparedStmt.setString(7, user.getImagePath());
-                preparedStmt.setString(8, user.getUserType());
+   public boolean updateUsersAllFields(User user, String condition) {
+    String st = DataValidation.userValidationTest(user);
+    if ("Valid".equals(st)) {
+        StringBuilder queryBuilder = new StringBuilder("UPDATE users SET firstName = ?, lastName = ?, username = ?, ");
+        queryBuilder.append("phone = ?, email = ?, userPassword = ?, image = ?, userType = ?");
+        queryBuilder.append(" ").append(condition);
 
-                preparedStmt.executeUpdate();
-                setStatus("User was updated successfully");
-                return true;
-            } catch (SQLException e) {
-                setStatus("Couldn't update user");
-                return false;
-            }
-        } else {
-            setStatus(st);
-            return false;
-        }
-    }
+        try (PreparedStatement preparedStmt = connection.prepareStatement(queryBuilder.toString())) {
+            preparedStmt.setString(1, user.getFirstName());
+            preparedStmt.setString(2, user.getLastName());
+            preparedStmt.setString(3, user.getUsername());
+            preparedStmt.setString(4, user.getPhoneNumber());
+            preparedStmt.setString(5, user.getEmail());
+            preparedStmt.setString(6, user.getPassword());
+            preparedStmt.setString(7, user.getImagePath());
+            preparedStmt.setString(8, user.getUserType());
 
-    public boolean updateUserSingleField(String fieldName, String newValue, String condition) {
-        if (!isValidColumnName(fieldName)) {
-            setStatus("Invalid column name");
-            return false;
-        }
-
-        String query = "UPDATE users SET " + fieldName + " = ? " + condition;
-        try (PreparedStatement preparedStmt = connection.prepareStatement(query)) {
-            preparedStmt.setString(1, newValue);
             preparedStmt.executeUpdate();
-            setStatus("User " + fieldName + " was updated successfully");
+            setStatus("User was updated successfully");
             return true;
         } catch (SQLException e) {
-            setStatus("Couldn't update user " + fieldName);
+            setStatus("Couldn't update user");
             return false;
         }
+    } else {
+        setStatus(st);
+        return false;
     }
+}
+
+
+   public boolean updateUserSingleField(String fieldName, String newValue, String condition) {
+    if (!isValidColumnName(fieldName)) {
+        setStatus("Invalid column name");
+        return false;
+    }
+
+    StringBuilder queryBuilder = new StringBuilder("UPDATE users SET ");
+    queryBuilder.append(fieldName).append(" = ? ").append(condition);
+
+    try (PreparedStatement preparedStmt = connection.prepareStatement(queryBuilder.toString())) {
+        preparedStmt.setString(1, newValue);
+        preparedStmt.executeUpdate();
+        setStatus("User " + fieldName + " was updated successfully");
+        return true;
+    } catch (SQLException e) {
+        setStatus("Couldn't update user " + fieldName);
+        return false;
+    }
+}
 
     private boolean isValidColumnName(String columnName) {
         return columnName.matches("[a-zA-Z0-9_]+");
